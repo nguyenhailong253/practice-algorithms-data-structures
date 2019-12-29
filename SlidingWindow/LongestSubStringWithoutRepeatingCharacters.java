63df130860455143f7890c72b0cc701cf5783f58
Long Nguyen <nguyenhailong253@gmail.com>
2019-12-29 19:46:30 +1100

    add comments in longest substring code

--- SlidingWindow/LongestSubStringWithoutRepeatingCharacters.java
+++ SlidingWindow/LongestSubStringWithoutRepeatingCharacters.java
@@ -4,7 +4,10 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * O(2n) = O(n) complexity
+ * O(2n) = O(n) time complexity
+ * 
+ * Using Hash set and use its property of storing unique value to track non
+ * repeating character
  */
 
 class Solution {
@@ -23,22 +26,25 @@ class Solution {
         int endWindowIdx = 0;
 
         while (endWindowIdx < s.length()) {
+            // If hash set does not contain char at end window index, add it
             if (!set.contains(s.charAt(endWindowIdx))) {
                 set.add(s.charAt(endWindowIdx));
                 endWindowIdx++;
+                // Update max substring length with current one or window length
                 maxSubstringLength = Math.max(endWindowIdx - startWindowIdx, maxSubstringLength);
             } else {
                 set.remove(s.charAt(startWindowIdx));
                 startWindowIdx++;
             }
         }
-
         return maxSubstringLength;
     }
 }
 
 /**
  * O(n) time complexity O(n) space complexity
+ * 
+ * Using hash map to store char as key, and its LAST SEEN index as value
  */
 
 class Solution {
@@ -52,26 +58,27 @@ class Solution {
         int maxSubstringLength = Integer.MIN_VALUE;
 
         Map<Character, Integer> map = new HashMap<>();
-
         int startWindowIdx = 0;
 
         for (int endWindowIdx = 0; endWindowIdx < s.length(); endWindowIdx++) {
 
             if (map.containsKey(s.charAt(endWindowIdx))) {
+                // If already contains char, move the start index to the last
+                // seen position of this char
                 startWindowIdx = Math.max(startWindowIdx, map.get(s.charAt(endWindowIdx)));
             }
-
             maxSubstringLength = Math.max(endWindowIdx - startWindowIdx + 1, maxSubstringLength);
+            // Update current char in map, with last seen position (which is current index)
             map.put(s.charAt(endWindowIdx), endWindowIdx + 1);
         }
-
         return maxSubstringLength;
     }
 }
 
 /**
- * O(n) time complexity Assume ASCII characters => array size is 128 O(1) space
- * complexity
+ * O(n) time complexity O(1) space complexity
+ * 
+ * Assume ASCII characters => array size is 128
  */
 
 class Solution {
@@ -85,11 +92,9 @@ class Solution {
         int maxSubstringLength = Integer.MIN_VALUE;
 
         int[] characterIndex = new int[128];
-
         int startWindowIdx = 0;
 
         for (int endWindowIdx = 0; endWindowIdx < s.length(); endWindowIdx++) {
-
             startWindowIdx = Math.max(startWindowIdx, characterIndex[s.charAt(endWindowIdx)]);
 
             maxSubstringLength = Math.max(endWindowIdx - startWindowIdx + 1, maxSubstringLength);
